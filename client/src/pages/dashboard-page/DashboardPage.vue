5fe82c2d321c2a1d3578737d2eb157238258bc44
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-16 09:54:20 +0200

    feat: dashboard design done

--- client/src/pages/dashboard-page/DashboardPage.vue
+++ client/src/pages/dashboard-page/DashboardPage.vue
@@ -1,13 +1,51 @@
 <template>
-  
+    <v-main>
+        <v-container fluid>
+        <v-row align="center" justify="space-around">
+            <create-ip-modal/>
+            <create-request-modal/>
+            <view-request-modal/>
+        </v-row>
+        </v-container>
+        
+        <dashboard-calander />
+    </v-main>
 </template>
 
 <script>
+import CreateIPModal from './CreateIPModal';
+import CreateRequestModal from './CreateRequestModal';
+import DashboardCalander from './DashboardCalander'
+import ViewRequestModal from '@/components/shared/ViewRequestModal'
 export default {
-
-}
+    name: 'DashboardPage',
+    components: {
+        'dashboard-calander': DashboardCalander,
+        'create-ip-modal': CreateIPModal,
+        'create-request-modal': CreateRequestModal,
+        'view-request-modal': ViewRequestModal
+    },
+    data() {
+        return {
+            ipModal: false,
+            requestModal: false
+        }
+    },
+    methods: {
+        closeIPModal() {
+            this.ipModal = false;
+        },
+        closeRequestModal() {
+            this.requestModal = false;
+        },
+        openIPModal() {
+            this.ipModal = true;
+        },
+        openRequestModal() {
+            this.requestModal = true;
+        }
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
