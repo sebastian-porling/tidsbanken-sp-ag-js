9e13d25c1160e9012afbee885c0f7c79f570c51c
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-09-16 16:17:06 +0200

    Started on methods in VacationController endpoint

--- server/server/src/main/java/se/experis/tidsbanken/server/controllers/CommentController.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/controllers/CommentController.java
@@ -1,34 +1,71 @@
 package se.experis.tidsbanken.server.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import se.experis.tidsbanken.server.models.*;
+import se.experis.tidsbanken.server.repositories.CommentRepository;
 
 @Controller
 public class CommentController{
+
+    @Autowired
+    private CommentRepository commentRepository;
+
     @GetMapping("/request/:request_id/comment")
     public ResponseEntity<CommonResponse> getComments(){
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+
+       /*
+        */
+
+        return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
     @PostMapping("/request/:request_id/comment")
     public ResponseEntity<CommonResponse> createComment(@RequestBody Comment comment) {
-        Comment createComment = commentRepository.save(comment);
-        Response response = new Response(createComment, "CREATE");
-        HttpStatus status = HttpStatus.CREATED;
-        return new ResponseEntity<>(response, status);
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+
+        /*
+         */
+
+        return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
     @GetMapping("/request/:request_id/comment/{comment_id}")
-    public ResponseEntity<CommonResponse> getComments(){
+    public ResponseEntity<CommonResponse> getComment(){
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+
+        /*
+         */
+
+        return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
     @PatchMapping("/request/:request_id/comment/{comment_id}")
-    public ResponseEntity<CommonResponse> updateComment(@RequestBody Comment comment) {
+    public ResponseEntity<CommonResponse> updateComment(@RequestBody Comment comment) {CommonResponse cr = new CommonResponse();
+        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+
+        /*
+         */
+
+        return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
     @DeleteMapping("/request/:request_id/comment/{comment_id}")
     public ResponseEntity<CommonResponse> deleteComment(){
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+
+        /*
+         */
+
+        return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 }
--- server/server/src/main/java/se/experis/tidsbanken/server/repositories/VacationRequestRepository.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/repositories/VacationRequestRepository.java
@@ -1,8 +1,11 @@
 package se.experis.tidsbanken.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import se.experis.tidsbanken.server.models.AppUser;
 import se.experis.tidsbanken.server.models.VacationRequest;
 
 public interface VacationRequestRepository extends JpaRepository<VacationRequest, Long> {
     VacationRequest getById(long id);
+
+    VacationRequest findAllByOwner(AppUser owner);
 }
