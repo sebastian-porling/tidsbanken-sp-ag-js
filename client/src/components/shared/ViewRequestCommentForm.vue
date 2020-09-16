70e27eefcc778a1df2c6af72aa695ad05cb04e83
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-16 09:56:29 +0200

    feat: comments design done

--- client/src/components/shared/ViewRequestCommentForm.vue
+++ client/src/components/shared/ViewRequestCommentForm.vue
@@ -1,13 +1,18 @@
 <template>
-  
+    <div>
+        <v-textarea solo name="input-7-4" label="Comment"></v-textarea>
+        <v-container class="d-flex flex-row-reverse">
+            <v-btn color="green darken-1" text>
+                Submit
+            </v-btn>
+        </v-container>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    name: 'ViewRequestCommentForm'
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
