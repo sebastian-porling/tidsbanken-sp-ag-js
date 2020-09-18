72b616884fc7369279b2bc0d2cef58ddbd41e598
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-18 07:58:39 +0200

    feat: added sql method

--- server/server/src/main/java/se/experis/tidsbanken/server/repositories/VacationRequestRepository.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/repositories/VacationRequestRepository.java
@@ -1,10 +1,10 @@
 package se.experis.tidsbanken.server.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import se.experis.tidsbanken.server.models.AppUser;
+import se.experis.tidsbanken.server.models.User;
 import se.experis.tidsbanken.server.models.VacationRequest;
 
 import java.util.List;
@@ -12,6 +12,7 @@ import java.util.List;
 @Repository
 public interface VacationRequestRepository extends JpaRepository<VacationRequest, Long> {
 
-    List<VacationRequest> findAllByOwner(AppUser owner);
+    List<VacationRequest> findAllByOwner(User owner);
 
+    List<VacationRequest> findAllByOrderByStartDesc(Pageable pageable);
 }
