01aec94f6a596833d097ef2baa790d74509e3a8a
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-18 08:00:31 +0200

    feat: added paging

--- server/server/src/main/java/se/experis/tidsbanken/server/controllers/VacationController.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/controllers/VacationController.java
@@ -1,19 +1,18 @@
 package se.experis.tidsbanken.server.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import se.experis.tidsbanken.server.models.*;
 import se.experis.tidsbanken.server.repositories.VacationRequestRepository;
+import se.experis.tidsbanken.server.services.AuthorizationService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 public class VacationController{
@@ -21,60 +20,38 @@ public class VacationController{
     @Autowired
     private VacationRequestRepository vacationRequestRepository;
 
+    @Autowired
+    private AuthorizationService authorizationService;
+
     @GetMapping("/request")
-    public ResponseEntity<CommonResponse> getRequests(){
-        /*      X    User should be authenticated
-                O    Curated list
-                O    Chronological order (soonest first)
-                O    Return first 50 by default
-                X    All users can see: All approved requests
-                X    All users can see: Their own request, regardless of state
-                X    Admin can see: All request, regardless of state
-         */
-        CommonResponse cr;
-        HttpStatus resStatus;
-
-        List<VacationRequest> allVacationRequests = vacationRequestRepository.findAll(Sort.by(Sort.Direction.DESC, "period_start"));
-        List<VacationRequest> firstN = new ArrayList<>();
-        List<HashMap> responseData = new ArrayList<>();
-
-        int n = 50;
-
-        if(allVacationRequests.size() > 0) {
-            if (allVacationRequests.size() < n) n = allVacationRequests.size();
-
-            for (int i = 0; i < n; i++) {
-                firstN.add(allVacationRequests.get(i));
-            }
-
-            for (VacationRequest request : firstN) {
-                HashMap<String, Object> data = new HashMap<>();
-                data.put("title", request.title);
-                data.put("period_start", request.start);
-                data.put("period_end", request.end);
-                data.put("owner", request.owner.full_name);
-                data.put("request_status", request.requestStatus);
-                data.put("created_at", request.created_at);
-
-                if(request.moderator != null) {
-                    data.put("moderator", request.moderator.full_name);
-                    data.put("moderation_date", request.moderation_date);
-                }
-
-                responseData.add(data);
-            }
-            cr = new CommonResponse("Success", responseData);
-            resStatus = HttpStatus.OK;
+    public ResponseEntity<CommonResponse> getRequests(HttpServletRequest request,
+                                                      @RequestParam("date") Optional<String> date,
+                                                      @RequestParam("page") Optional<Integer> page,
+                                                      @RequestParam("amount") Optional<Integer> amount){
+        /*
+        * TODO
+        * [ ] - Add date search
+        * [ } - Add between two dates search
+        */
+        List<VacationRequest> allVacationRequests = vacationRequestRepository
+                .findAllByOrderByStartDesc(PageRequest.of(page.orElse(0), amount.orElse(50)));
+        if (!authorizationService.isAuthorized(request)) { return unauthorized(); }
+        if (authorizationService.currentUser(request).isAdmin()) {
+            return ResponseEntity.ok(new CommonResponse(
+                    "All vacation requests", allVacationRequests));
         } else {
-            cr = new CommonResponse("No request found");
-            resStatus = HttpStatus.NOT_FOUND;
+            return ResponseEntity.ok(new CommonResponse(
+                    "All vacation requests",
+                    allVacationRequests.stream().filter((vr) ->
+                    vr.getOriginalOwner().getId()
+                            .equals(authorizationService.currentUser(request).getId()))
+                    .collect(Collectors.toList())));
         }
-
-        return new ResponseEntity<>(cr, resStatus);
     }
 
     @PostMapping("/request")
-    public ResponseEntity<CommonResponse> createRequest(@RequestBody VacationRequest vacationrequest) {
+    public ResponseEntity<CommonResponse> createRequest(@RequestBody VacationRequest vacationrequest,
+                                                        HttpServletRequest request) {
         CommonResponse cr = new CommonResponse();
         HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
 
@@ -87,8 +64,9 @@ public class VacationController{
         return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
-    @GetMapping("/request/:request_id")
-    public ResponseEntity<CommonResponse> getRequestsForId(@PathVariable("request_id")long request_id){
+    @GetMapping("/request/{request_id}")
+    public ResponseEntity<CommonResponse> getRequestsForId(@PathVariable("request_id") Long request_id,
+                                                           HttpServletRequest request){
         CommonResponse cr = new CommonResponse();
         HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
 
@@ -101,8 +79,10 @@ public class VacationController{
         return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
-    @PatchMapping("/request/:request_id")
-    public ResponseEntity<CommonResponse> updateRequest(@RequestBody VacationRequest vacationrequest) {
+    @PatchMapping("/request/{request_id}")
+    public ResponseEntity<CommonResponse> updateRequest(@PathVariable("request_id") Long requestId,
+                                                        @RequestBody VacationRequest vacationRequest,
+                                                        HttpServletRequest request) {
         CommonResponse cr = new CommonResponse();
         HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
 
@@ -120,17 +100,26 @@ public class VacationController{
         return new ResponseEntity<CommonResponse>(cr, resStatus);
     }
 
-    @DeleteMapping("/request/:request_id")
-    public ResponseEntity<CommonResponse> deleteRequest(@PathVariable("request_id")long request_id){
-        CommonResponse cr = new CommonResponse();
-        HttpStatus resStatus = HttpStatus.NOT_IMPLEMENTED;
+    @DeleteMapping("/request/{request_id}")
+    public ResponseEntity<CommonResponse> deleteRequest(@PathVariable("request_id") Long requestId,
+                                                        HttpServletRequest request){
+        if (authorizationService.isAuthorizedAdmin(request)) {
+            if (vacationRequestRepository.existsById(requestId)) {
+                vacationRequestRepository.deleteById(requestId);
+                return ResponseEntity.ok(new CommonResponse("Deleted"));
+            } else {
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .body(new CommonResponse("Vacation Request not found"));
+            }
+        } else {
+            return unauthorized();
+        }
+    }
 
-       /*      X     Search for existing request
-               X     If admin
-                            X     delete
-        */
-
-        return new ResponseEntity<CommonResponse>(cr, resStatus);
-
+    private ResponseEntity<CommonResponse> unauthorized() {
+        return new ResponseEntity<>(
+                new CommonResponse( "Not Authorized"),
+                HttpStatus.UNAUTHORIZED);
     }
 }
