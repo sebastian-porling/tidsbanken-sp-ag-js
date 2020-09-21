230442f59450ad0035162aaa30782d6b0f197165
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-21 13:44:03 +0200

    feat: added response builder

---
+++ server/server/src/main/java/se/experis/tidsbanken/server/utils/ResponseUtility.java
@@ -0,0 +1,43 @@
+package se.experis.tidsbanken.server.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import se.experis.tidsbanken.server.models.CommonResponse;
+
+@Component
+public class ResponseUtility {
+
+    public ResponseEntity<CommonResponse> unauthorized() {
+        return buildResponse(HttpStatus.UNAUTHORIZED, "Not Authorized");
+    }
+
+    public ResponseEntity<CommonResponse> forbidden() {
+        return buildResponse(HttpStatus.FORBIDDEN, "Forbidden");
+    }
+
+    public ResponseEntity<CommonResponse> errorMessage() {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+                "Something went wrong when trying to process the request");
+    }
+
+    public ResponseEntity<CommonResponse> notFound(String message) {
+        return buildResponse(HttpStatus.NOT_FOUND, message);
+    }
+
+    public ResponseEntity<CommonResponse> badRequest(String message) {
+        return buildResponse(HttpStatus.BAD_REQUEST, message);
+    }
+
+    public ResponseEntity<CommonResponse> ok(String message, Object data) {
+        return ResponseEntity.ok().body(new CommonResponse().message(message).data(data));
+    }
+
+    public ResponseEntity<CommonResponse> created(String message, Object data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponse().message(message).data(data));
+    }
+
+    private ResponseEntity<CommonResponse> buildResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status).body(new CommonResponse().message(message));
+    }
+}
