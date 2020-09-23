4ed66fbe5ae94ffffe633c5c50107e84e315f903
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-23 09:45:44 +0200

    feat: added 2fa for sign in

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -12,6 +12,7 @@ export const store = new Vuex.Store({
         token: localStorage.getItem('access_token') || null,
         user: localStorage.getItem('user') || null,
         requestHistory: [],
+        qrCode: null
     },
     getters: {
         loggedIn(state) {
@@ -29,7 +30,10 @@ export const store = new Vuex.Store({
             state.currentUser = user.user
             state.token = user.token
         },
-        destoyToken(state) {
+        multiAuthQrCode(state, qrCode) {
+            state.qrCode = qrCode;
+        },
+        destroyToken(state) {
             state.token = null;
         },
         setRequestHistory(state, requests) {
@@ -41,18 +45,34 @@ export const store = new Vuex.Store({
             return new Promise((resolve, reject) => {
                 axios.post('login', {
                     email: credentials.email,
-                    password: credentials.password
+                    password: credentials.password,
+                    code: credentials.code
                 })
                 .then(response => {
-                    const user = response.data.data;
-                    localStorage.setItem('access_token', user.token);
-                    localStorage.setItem('user', JSON.stringify(user.user));
-                    context.commit('retrieveCurrentUser', user)
+                    const data = response.data.data;
+                    localStorage.setItem('access_token', data.token);
+                    localStorage.setItem('user', JSON.stringify(data.user));
+                    context.commit('retrieveCurrentUser', data);
                     resolve(response);
                 })
                 .catch (error => {
-                    console.log(error);
-                    reject(error);
+                    reject(error.response);
+                })
+            })
+        },
+        retrieveQr(context, credentials) {
+            return new Promise((resolve, reject) => {
+                axios.post('login', {
+                    email: credentials.email,
+                    password: credentials.password
+                })
+                .then(response => {
+                    const data = response.data.data;
+                    context.commit('multiAuthQrCode', data);
+                    resolve(response);
+                })
+                .catch (error => {
+                    reject(error.response);
                 })
             })
         },
@@ -60,7 +80,7 @@ export const store = new Vuex.Store({
             if(context.getters.loggedIn) {
                 localStorage.removeItem('access_token');
                 localStorage.removeItem('user');
-                context.commit('destoyToken');
+                context.commit('destroyToken');
             }
         },
         retrieveRequestHistory(context, userid) {
