cfd245843cb6963170646187f8005d4ef37fd7a2
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-23 09:48:58 +0200

    update: added the second form

--- client/src/pages/login-page/LoginPage.vue
+++ client/src/pages/login-page/LoginPage.vue
@@ -1,15 +1,34 @@
 <template>
-  <LoginForm />
+<v-row justify="center" align="center">
+  <LoginForm v-if="!multiAuth" @loginMultiAuth="loginMultiAuth"/>
+  <LoginMultiAuthForm v-else :email="email" :password="password"/>
+</v-row>
 </template>
 
 <script>
 import LoginForm from './LoginForm';
+import LoginMultiAuthForm from './LoginMultiAuthForm';
 
 export default {
   name: 'LoginPage',
   components: {
-    LoginForm
-  }
+    LoginForm,
+    LoginMultiAuthForm
+  },
+  data() {
+    return {
+      multiAuth: false,
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginMultiAuth(email, password) {
+      this.multiAuth = true;
+      this.email = email;
+      this.password = password;
+    }
+  },
 }
 </script>
 
