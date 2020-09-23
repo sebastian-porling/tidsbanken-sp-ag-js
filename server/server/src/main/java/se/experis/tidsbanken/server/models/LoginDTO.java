e8bd687f51e5d85c5c6b137332a8fdd72f3a0685
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-23 10:56:32 +0200

    update: user_id to id

--- server/server/src/main/java/se/experis/tidsbanken/server/models/LoginDTO.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/models/LoginDTO.java
@@ -17,7 +17,7 @@ public class LoginDTO {
 
     public HashMap<String, Object> getUser() {
         final HashMap<String, Object> userInfo = new HashMap<>();
-        userInfo.put("user_id", user.getId());
+        userInfo.put("id", user.getId());
         userInfo.put("full_name", user.getFullName());
         userInfo.put("profile_pic", user.getProfilePic());
         userInfo.put("is_admin", user.isAdmin());
