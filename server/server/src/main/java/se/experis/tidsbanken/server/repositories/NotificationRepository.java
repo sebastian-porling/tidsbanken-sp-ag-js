2dd4f15658b0d927dd89ec04f036c3099a2b05ec
Sebastian Porling <sebastian.porling@gmail.com>
2020-09-25 21:49:46 +0200

    feat: add socket repo and entity

--- server/server/src/main/java/se/experis/tidsbanken/server/repositories/NotificationRepository.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/repositories/NotificationRepository.java
@@ -2,6 +2,11 @@ package se.experis.tidsbanken.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.experis.tidsbanken.server.models.Notification;
+import se.experis.tidsbanken.server.models.User;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUser(User user);
+    List<Notification> findAllByOrderByIdDesc();
 }
