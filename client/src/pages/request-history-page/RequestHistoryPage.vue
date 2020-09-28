66e0e29dcf39f7a16ddf1d0c660f022505ad55b5
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-09-28 08:43:33 +0200

    fix: router param to request history

--- client/src/pages/request-history-page/RequestHistoryPage.vue
+++ client/src/pages/request-history-page/RequestHistoryPage.vue
@@ -1,51 +1,57 @@
 <template>
-<v-col class="container">
-  <br>
-  <br>
-  <div v-if="user">
-    <v-row>
-    <UserInfoComponent :user="user" />
-  </v-row>
-  <br>
-  <v-row>
-    <RequestHistoryTable :user="user" />
-  </v-row>
-  </div>
-  <div v-if="!user">
-    <h1>Something went wrong..</h1>
-  </div>
-</v-col>
+  <v-col class="container">
+    <br />
+    <br />
+    <div v-if="user">
+      <v-row>
+        <UserInfoComponent :user="user" />
+      </v-row>
+      <br />
+      <v-row>
+        <RequestHistoryTable :user="user" />
+      </v-row>
+    </div>
+    <div v-if="!user">
+      <h1>Something went wrong..</h1>
+    </div>
+  </v-col>
 </template>
 
 <script>
-import UserInfoComponent from './UserInfoComponent';
-import RequestHistoryTable from './RequestHistoryTable';
+import UserInfoComponent from "./UserInfoComponent";
+import RequestHistoryTable from "./RequestHistoryTable";
 
 export default {
-  name: 'RequestHistoryPage',
+  name: "RequestHistoryPage",
   components: {
     UserInfoComponent,
-    RequestHistoryTable
+    RequestHistoryTable,
   },
   props: {
     id: {
-      type: Number,
-      required: true
-    }
+      type: String,
+      required: true,
+    },
   },
   created() {
     this.$store.dispatch("retrieveAllUsers");
   },
   computed: {
+    currentUser() {
+      return this.$store.getters.getCurrentUser;
+    },
     user() {
-      return this.$store.getters.getAllUsers.find(
-        user => user.id === this.id
-      )
-    }
-  }
-}
+      if (this.currentUser.is_admin) {
+        return this.$store.getters.getAllUsers.find(
+          (user) => user.id === parseInt(this.id)
+        )
+      } else {
+        return this.currentUser;
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
