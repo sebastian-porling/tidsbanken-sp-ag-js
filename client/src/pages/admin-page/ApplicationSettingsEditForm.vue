f9a0ee53e51bdc64e783b93211d66ad967b09fe5
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-09-29 10:45:35 +0200

    feat: error and validation for settings modal

--- client/src/pages/admin-page/ApplicationSettingsEditForm.vue
+++ client/src/pages/admin-page/ApplicationSettingsEditForm.vue
@@ -4,28 +4,30 @@
             <span class="headline">Edit Application Setting</span>
         </v-card-title>
         <v-card-text>
-            <v-container>
+            <v-form v-model="valid">
                 <v-row justify="end">
                     <v-col cols="12" sm="6" md="6">
                         <v-text-field
                             label="Setting"
                             v-model="setting.key"
+                            :rules="keyRules"
                         ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="6">
                         <v-text-field
                             label="Value"
                             v-model="setting.value"
+                            :rules="valueRules"
                         ></v-text-field>
                     </v-col>
                     
                 </v-row>
-            </v-container>
+            </v-form>
         </v-card-text>
         <v-card-actions >
             <v-row justify="end" style="margin-right: 10px;">
                 <v-btn color="red darken-1" text @click="closeEdit">Cancel</v-btn>
-                <v-btn color="green" text v-on:click="editSetting">Edit</v-btn>
+                <v-btn color="green" text v-on:click="editSetting" :disabled="!valid">Edit</v-btn>
             </v-row>
         </v-card-actions>
     </v-card>
@@ -35,6 +37,17 @@
 export default {
     name: 'ApplicationSettingsEditForm',
     props: ['setting'],
+    data() {
+        return {
+            valid: true,
+            keyRules: [
+                (v) => !!v || "Field is required",
+            ],
+            valueRules: [
+                (v) => !!v || "Field is required",
+            ],
+        }
+    },
     methods: {
         editSetting() {
             if (this.setting.key && this.setting.value) {
--- client/src/pages/admin-page/ApplicationSettingsInfo.vue
+++ client/src/pages/admin-page/ApplicationSettingsInfo.vue
@@ -4,23 +4,25 @@
             <span class="headline">Application Settings</span>
         </v-card-title>
         <v-card-text>
-            <v-container>
+            <v-form v-model="valid">
                 <v-row justify="end">
                     <v-col cols="12" sm="6" md="6">
                         <v-text-field
                             label="Setting"
                             v-model="key"
+                            :rules="keyRules"
                         ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="6">
                         <v-text-field
                             label="Value"
                             v-model="value"
+                            :rules="valueRules"
                         ></v-text-field>
                     </v-col>
-                    <v-btn color="green" text v-on:click="addSetting">Add</v-btn>
+                    <v-btn color="green" text v-on:click="addSetting" :disabled="!valid">Add</v-btn>
                 </v-row>
-            </v-container>
+            </v-form>
 
             <v-data-table
                 :headers="headers"
@@ -48,12 +50,19 @@ export default {
     name: 'ApplicationSettingsInfo',
     data() {
         return {
+            valid: true,
             key: null,
             value: null,
             headers: [
                 { text: 'Setting', align: 'start', value: 'key'},
                 { text: 'Value', value: 'value' },
                 { text: 'Actions', value: 'actions', sortable: false },
+            ],
+            keyRules: [
+                (v) => !!v || "Field is required",
+            ],
+            valueRules: [
+                (v) => !!v || "Field is required",
             ],
         };
     },
