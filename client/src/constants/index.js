ada9f8b8b3c95ae5ada38f6cf4a8515dc7f777dc
Sebastian Porling <sebastian.porling@gmail.com>
2020-10-06 13:40:35 +0200

    update: change API url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,5 +1,5 @@
-const API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:3400/api/" : "/";
-const SOCKET_URL = process.env.NODE_ENV === 'development' ? "http://localhost:4121" : ":4121/";
+const API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:3400/api/" : "/api/";
+const SOCKET_URL = process.env.NODE_ENV === 'development' ? "http://localhost:4121" : ":4121";
 const headers = (token) => {return {headers: {authorization: `Bearer ${token}`}}};
 
 export {
