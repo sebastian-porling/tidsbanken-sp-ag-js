75a00b514cef065911d743fe3390eabd69adfb6a
Sebastian Porling <sebastian.porling@gmail.com>
2020-10-07 22:10:10 +0200

    feat: changed to properties variables, ssl

--- server/server/src/main/java/se/experis/tidsbanken/server/ServerApplication.java
+++ server/server/src/main/java/se/experis/tidsbanken/server/ServerApplication.java
@@ -3,24 +3,41 @@ package se.experis.tidsbanken.server;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 @SpringBootApplication
 public class ServerApplication {
 
-	private String host = "172.31.28.73";
-	private Integer port = 4121;
+	@Value("${server.host}")
+	private String host;
+	@Value("${server.ssl.key-store-password}")
+	private String keyPassword;
+	@Value("${socket.port}")
+	private Integer port;
 
 	@Bean
 	public SocketIOServer socketIOServer() {
 		Configuration config = new Configuration();
-		config.setHostname(host);
-		config.setPort(port);
-		SocketConfig socketConfig = new SocketConfig();
-		socketConfig.setReuseAddress(true);
-		config.setSocketConfig(socketConfig);
+		try {
+
+			config.setHostname(host);
+			config.setPort(port);
+			SocketConfig socketConfig = new SocketConfig();
+			socketConfig.setReuseAddress(true);
+			config.setKeyStorePassword(keyPassword);
+			InputStream stream = null;
+			stream = new FileInputStream("keystore.p12");
+			config.setKeyStore(stream);
+			config.setSocketConfig(socketConfig);
+		} catch (Exception e) {
+			System.out.println();
+		}
 		return new SocketIOServer(config);
 	}
 
